ec9f5c3291c4fc6f04b46f5c60fe9305f430c178
seiler-emerson <seiler.emerson@gmail.com>
2022-05-13 21:16:31 -0300

    aula 12

--- Aula11 - 01 - PraticaEmSala/src/br/com/entra21/aula11/pratica/em/sala/Main.java
+++ Aula11 - 01 - PraticaEmSala/src/br/com/entra21/aula11/pratica/em/sala/Main.java
@@ -41,9 +41,13 @@ public class Main {
 			case 5:
 				exibirMaisNovo();
 				break;
+			case 8:
+				exerciseOito();
+				break;
 			case 9:
 				mediaAlunos();
 				break;
+				
 			default:
 				System.out.println("Por favor, escolha uma opção válida");
 				break;
@@ -73,19 +77,19 @@ public class Main {
 	}
 
 	public static void sumAge() {
-		int sumAge=0;
+		int sumAge = 0;
 		byte age;
 		Scanner input = new Scanner(System.in);
-		
-		for(byte count=1;count<=20;count++) {
-			System.out.println("No momento a soma está em "+sumAge);
+
+		for (byte count = 1; count <= 20; count++) {
+			System.out.println("No momento a soma está em " + sumAge);
 			System.out.println("Por favor, insira a idade: ");
 			age = input.nextByte();
 			sumAge = sumAge + age;
 		}
-		System.out.println("A soma total das idades foi "+sumAge);
+		System.out.println("A soma total das idades foi " + sumAge);
 	}
-	
+
 	public static void maioresDeIdade() {
 		System.out.println("Exibir apenas os maiores de idade");
 		byte maiores = 0;
@@ -100,61 +104,83 @@ public class Main {
 				System.out.println("A última pessoa informada era maior de idade.");
 			}
 		}
-		System.out.println("Entre "+(count-1)+" pessoas, apenas "+maiores+" são maiores de idade.");
+		System.out.println("Entre " + (count - 1) + " pessoas, apenas " + maiores + " são maiores de idade.");
 	}
-	
+
 	public static void exibirMaisNovo() {
 		Scanner input = new Scanner(System.in);
-		String name, youngerName="";
-		byte age, ageMinimal=0;
-		
-		for(byte count=1;count<=10;count++) {
+		String name, youngerName = "";
+		byte age, ageMinimal = 0;
+
+		for (byte count = 1; count <= 10; count++) {
 			System.out.println("Digite o nome: ");
 			name = input.next();
-			System.out.println("Digite sua idade "+name);
+			System.out.println("Digite sua idade " + name);
 			age = input.nextByte();
-			
-			//no primeiro laco sempre a primeira idade vai ser a menor
+
+			// no primeiro laco sempre a primeira idade vai ser a menor
 			if (count == 1) {
 				ageMinimal = age;
 				youngerName = name;
-				System.out.println("Sou o primeiro, por isso sou o mais novo "+name+" "+age);
+				System.out.println("Sou o primeiro, por isso sou o mais novo " + name + " " + age);
 			} else {
-				//a partir do segundo laco comparar com ageMinimal
-				System.out.println("Estou no laço "+count+" e preciso verificar.");
-				if(age<ageMinimal) {
-					System.out.println("Ainda bem que testei porque a pessoa mais nova é "+name+" com "+age);
+				// a partir do segundo laco comparar com ageMinimal
+				System.out.println("Estou no laço " + count + " e preciso verificar.");
+				if (age < ageMinimal) {
+					System.out.println("Ainda bem que testei porque a pessoa mais nova é " + name + " com " + age);
 					ageMinimal = age;
 					youngerName = name;
 				}
 			}
 		}
-		System.out.println(youngerName+" tem "+ageMinimal+" e é o mais novo(a).");
+		System.out.println(youngerName + " tem " + ageMinimal + " e é o mais novo(a).");
 	}
-	
+
+	public static void exerciseOito() {
+
+		// Escreva um algoritmo que leia uma sequência de números do usuário e realize a
+		// soma desses números. Encerre a execução quando um número negativo for
+		// digitado.
+
+		Scanner inPut = new Scanner(System.in);
+		int number, sum = 0; // se não inicializa um valor na váriavel ela fica com lixo da memória.
+
+		do {
+
+			System.out.println("What number do you want?");
+			number = inPut.nextInt();
+			if (number >= 0) {
+				sum = sum + number;
+			} // somente números positivos serão somados, ou números naturais.
+
+		} while (number >= 0); // enquanto o número for maior que zero repita
+
+		System.out.println("Your sum is: " + sum);
+
+	}
+
 	public static void mediaAlunos() {
 		Scanner input = new Scanner(System.in);
-		byte qtd, contador=0;
-		float nota, soma=0f;
+		byte qtd, contador = 0;
+		float nota, soma = 0f;
 		String resposta;
 		System.out.println("Quantas notas você quer capturar? ");
 		qtd = input.nextByte();
-		
+
 		do {
 			contador++;
 			System.out.println("Informe uma nota");
 			nota = input.nextFloat();
-			if(contador>=qtd) {
+			if (contador >= qtd) {
 				System.out.println("Gostaria de capturar mais alguma nota?");
 				resposta = input.next();
-				if(resposta.equals("sim")) {
+				if (resposta.equals("sim")) {
 					contador--;
 				}
 			}
-			
-			
-			soma = soma+nota;
-		} while (contador<=qtd);
+
+			soma = soma + nota;
+		} while (contador <= qtd);
 		System.out.println(soma);
 	}
 
